7b2b2bd2976498ba4fa4d0535c5c8eab50f6dd4d
GAZİ TUSAS MYO <gazi.kazan.myo@outlook.com>
2020-02-24 12:11:26 +0300

    Add project files.

---
+++ Gazi.Appp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gazi.Appp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci a = new Ogrenci();
+
+            a.ad = "Ahmet Ruhi";
+            a.soyad = "Aydemir";
+            a.no = 193801006;
+            a.bolum = "PC";
+
+
+            Console.WriteLine(a.OgrenciBilgileri());
+            Console.ReadKey();
+        }
+    }
+
+    class Ogrenci
+    {
+        public int no;
+        public string ad;
+        public string soyad;
+        public string bolum;
+
+        public string OgrenciBilgileri()=> $"Adı:{this.ad}\nSoyadı:{this.soyad}\nNumara:{this.no}\nBölüm:{this.bolum}";
+        //{
+        //    return $"Adı:{this.ad}\nSoyadı:{this.soyad}\nNumara:{this.no}\nBölüm:{this.bolum}";
+        //}
+    }
+}
